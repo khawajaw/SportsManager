929e4accf59f99ebb83aa5d27660cc3ad3a72c39
Wajih Khawaja <khawajaw@wit.edu>
2019-06-20 14:15:37 -0400

    test

---
+++ app/src/main/java/com/example/sportsmanager/CreateNew.java
@@ -0,0 +1,13 @@
+package com.example.sportsmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new);
+    }
+}
